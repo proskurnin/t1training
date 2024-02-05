b25e923287a26e3336805b034555478dddc0c2db
Roman Proskurnin <roman@proskurnin.ru>
2024-02-05 16:20:41 +0300

    Home Work 01

--- src/main/java/ru/proskurnin/core/App.java
+++ src/main/java/ru/proskurnin/core/App.java
@@ -1,13 +1,9 @@
 package ru.proskurnin.core;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        TestRunner.runTests(ExampleTest.class);
     }
 }
